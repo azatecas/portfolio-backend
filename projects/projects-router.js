7e82101189876d8c00d3cc137dcfe020002f5189
Luis PM <luisp237@gmail.com>
2020-03-31 23:21:53 -0400

    fix endpoint to return array instead of object

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -5,12 +5,13 @@ router.get("/", (req, res) => {
     Projects
         .find()
         .then(proj => {
-            res.status(200).json({proj});
+            res.status(200).json(proj);
         })
         .catch(err => {
             console.log(err)
             res.status(500).json({error: err})
         })
 })
+
 
 module.exports = router;
