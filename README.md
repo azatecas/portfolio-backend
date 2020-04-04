64303adb09c3673799f377336d8deece14340461
Luis PM <luisp237@gmail.com>
2020-04-04 10:33:26 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # portfolio-backend
-backend for my porfolio RESTful api
+This is the BackEnd Portion of My [Web Portfolio](https://github.com/azatecas/portfolio-luis).
+
+Technologies used in this project:
+
+    Node.js
+    Express
+    Knex
+    Express-Rate-Limiter
+    CORS
+    Helmet
+    SQlite
+
+This Project is constantly evolving, any major updates will be reflect on the commits and the README
+
+
--- data/seeds/001-proj.js
+++ data/seeds/001-proj.js
@@ -9,7 +9,7 @@ exports.seed = function(knex) {
           img_url: 'https://i.imgur.com/nuRudAI.png',
           project_title: 'Portfolio',
           project_desc: 'Complete FullStack site created by me (Luis Pepen), as a way to showcase my front end and backend abilities, this site fetches data from a custom REST api',
-          tech_used: 'CSS, JavaScript, React, Node, Express, SQLite, Zeit',
+          tech_used: 'CSS, JavaScript, React, Custom Hooks, Node, Express, SQLite, Knex, Zeit',
           project_url: 'www.luisunlimited.com',
           github_repo: 'https://github.com/azatecas/portfolio-luis'
         },
@@ -18,7 +18,7 @@ exports.seed = function(knex) {
           img_url: 'https://i.imgur.com/r1ZuH0y.png',
           project_title: 'ATLA Fan Page',
           project_desc: 'This is a fan page that fetches data from an open Rest API. This project utilizes Axios and Client side pagination, it was later refactored to include more CSS animations',
-          tech_used: 'CSS, JavaScript, Axios, React, Redux, Particle.js, Zeit',
+          tech_used: 'CSS, JavaScript, Axios, React, React Router, Particle.js, Zeit',
           project_url: 'https://avatar-alpha.now.sh/',
           github_repo: 'https://github.com/azatecas/avatar-single-page-app'
         },
@@ -27,7 +27,7 @@ exports.seed = function(knex) {
           img_url: 'https://i.imgur.com/QzKKuEb.png',
           project_title: 'Water my plants',
           project_desc: 'This is a team Buildweek project in which i was assigned the role of React II Developer and had to add CRUD functionality with React and Redux to this project. Dashboard allows you to create, add, edit, and delete any plants in your dashboard',
-          tech_used: 'CSS, JavaScript, Axios, React, Redux, Auth Tokens, Zeit',
+          tech_used: 'CSS, JavaScript, Axios, React, React Router, Redux, Auth Tokens, Zeit',
           project_url: 'https://front-end-2-five.now.sh/dashboard',
           github_repo: 'https://github.com/Build-Week-Water-My-Plants-01/Front-End'
         },
@@ -82,16 +82,21 @@ exports.seed = function(knex) {
         },
         {
           id: 8,
+          skills_name: 'React Router',
+          img_url: 'https://cdn.worldvectorlogo.com/logos/react-router.svg'
+        },
+        {
+          id: 9,
           skills_name: 'NodeJS',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
         },
         {
-          id: 9,
+          id: 10,
           skills_name: 'SQLite',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/9/97/Sqlite-square-icon.svg'
         },
         {
-          id: 10,
+          id: 11,
           skills_name: 'PostgreSQL',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg'
         },        
