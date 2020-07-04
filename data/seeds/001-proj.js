2f607edec3e13ccedcc58dffe3f34794b31e5a4b
Luis PM <luisp237@gmail.com>
2020-07-04 06:13:33 -0400

    seeded new information about skills/projects'

--- data/seeds/001-proj.js
+++ data/seeds/001-proj.js
@@ -10,54 +10,72 @@ exports.seed = function(knex) {
           img_url: 'https://i.imgur.com/WB913Xt.png',
           project_title: 'Groa.us',
           project_desc: 'Contributor to an Open Source movie recommendation plattform, that serves unbias movie recommendations based on user ratings, I was part of an Agile team that concisted of stakeholders, UX designers, Full Stack Developers, and Data Scientist. I significantly contributed to the implementation of Okta Oauth2 Authentication and Social Login Providers, as well a fixing many bugs and streamlining performace',
-          tech_used: 'RESTful Architecture, OKTA,JAVASCRIPT, MATERIAL-UI, REACT, NODE, EXPRESS, POSTGRESQL, AWS AMPLIFY, AWS ELASTIC BEANSTALK',
-          project_url: 'https://www.groa.us',
+          tech_used: 'RESTful Architecture, AJAX, OKTA,JAVASCRIPT, MATERIAL-UI, REACT, NODE, EXPRESS, POSTGRESQL, AWS AMPLIFY, AWS ELASTIC BEANSTALK',
+          project_url: 'https://groa.us',
           github_repo: 'https://github.com/Lambda-School-Labs/Groa-fe'
         },
         {
           id: 2, 
-          img_url: 'https://i.imgur.com/nuRudAI.png',
-          project_title: 'Portfolio',
-          project_desc: 'Complete FullStack site created by me (Luis Pepen), as a way to showcase my front end and backend abilities, this site fetches my portfolio data from a custom REST api created with Node.js and Express',
-          tech_used: 'CSS, JavaScript, React, Custom Hooks, Node, Express, SQLite, Knex, Git, Zeit',
-          project_url: 'https://luispepen.com',
-          github_repo: 'https://github.com/azatecas/portfolio-luis'
+          img_url: 'https://i.imgur.com/uzrpbAj.png',
+          project_title: 'Game of Life',
+          project_desc: 'Reactjs implementation of John Conway’s game of life. Created an algorithm that evaluates each active cell and allows for that cell to live or die based on the Game of Life rules.',
+          tech_used: 'CSS, JavaScript, React, Git, Vercel( formely Zeit)',
+          project_url: 'https://game-of-life-sage.vercel.app/',
+          github_repo: 'https://github.com/azatecas/CS_Build_Week_1'
         },
         {
           id: 3, 
-          img_url: 'https://i.imgur.com/YleRQq5.png',
-          project_title: 'ATLA Information Page',
-          project_desc: 'This page contains character and episode data. The data is fetched from an open Rest API(link in the Docs). This project utilizes Axios for async requests and I implemented Client side pagination, it has been refactored to include more CSS animations',
-          tech_used: 'CSS, JavaScript, Axios, React, React Router, Particle.js, Git, Zeit',
-          project_url: 'https://avatar-alpha.now.sh/',
-          github_repo: 'https://github.com/azatecas/avatar-single-page-app'
+          img_url: 'https://i.imgur.com/nuRudAI.png',
+          project_title: 'Portfolio',
+          project_desc: 'Complete FullStack site created by me (Luis Pepen), as a way to showcase my front end and backend abilities, this site fetches my portfolio data from a custom REST api created with Node.js and Express',
+          tech_used: 'CSS, JavaScript, AJAX, React, Custom Hooks, Node, Express, SQLite, Knex, Git, Vercel( formely Zeit)',
+          project_url: 'https://luispepen.com',
+          github_repo: 'https://github.com/azatecas/portfolio-luis'
         },
         {
           id: 4, 
           img_url: 'https://i.imgur.com/QzKKuEb.png',
           project_title: 'Water My Plants',
           project_desc: 'This is a team Buildweek project in which I was assigned the role of React II Developer. I had to created a CRUD Web APP with full functionality using React and Redux to meet MVP. I created a Dashboard that allows the user to create, add, edit, and delete any plants in the Dashboard after login',
-          tech_used: 'CSS, JavaScript, Axios, React, React Router, Redux, Auth Tokens, Git, Zeit',
+          tech_used: 'CSS, JavaScript, Axios, AJAX, React, React Router, Redux, JWT, Git, Vercel( formely Zeit)',
           project_url: 'https://front-end-2-five.now.sh/dashboard',
           github_repo: 'https://github.com/Build-Week-Water-My-Plants-01/Front-End'
         },
         {
           id: 5, 
+          img_url: 'https://i.imgur.com/YleRQq5.png',
+          project_title: 'ATLA Information Page',
+          project_desc: 'This page contains character and episode data. The data is fetched from an open Rest API(link in the Docs). This project utilizes Axios for async requests and I implemented Client side pagination, it has been refactored to include more CSS animations',
+          tech_used: 'CSS, JavaScript, Axios, AJAX, React, React Router, Particle.js, Git, Vercel( formely Zeit)',
+          project_url: 'https://avatar-alpha.now.sh/',
+          github_repo: 'https://github.com/azatecas/avatar-single-page-app'
+        },
+        {
+          id: 6, 
+          img_url: 'https://i.imgur.com/mugtsnl.png',
+          project_title: 'Custom Github Users',
+          project_desc: 'Web App that fetches user(s) information from the Github API and presents it in a simplified and more pleasing theme. The content lazy loads and provides endless scrolling. You can search for any github user on the platform, see their information, public repos, and followers',
+          tech_used: 'CSS, JavaScript, AJAX, React, React Router, Redux, Git, Vercel( formely Zeit)',
+          project_url: 'https://github-api-test.vercel.app/',
+          github_repo: 'https://github.com/azatecas/github-api-test'
+        },
+        {
+          id: 7, 
           img_url: 'https://i.imgur.com/Xf9GXto.png',
           project_title: 'Sauti Studio',
           project_desc: 'Team buildweek application that allows you to design menu flows for lowend feature forms in emerging markets. My role was to create functional Login/signUp components, create a custom React Hook for form validation, and style the application using React styling library',
-          tech_used: 'React, Redux, Custom React Hooks, CSS, Javascript, Git, Zeit',
+          tech_used: 'React, Redux, Custom React Hooks, CSS, Javascript, Git, Vercel( formely Zeit)',
           project_url: 'https://sauti-styled.now.sh/',
           github_repo: 'https://github.com/Sauti-Studio3/Front-End'
         },
         {
-          id: 6, 
+          id: 8, 
           img_url: 'https://i.imgur.com/c63JJmt.png',
           project_title: 'Marketing Page UI',
           project_desc: 'This is a team Buildweek project in which i was assigned the role of Marketing UI Developer, I had to build and deploy a static Marketing Page UI for a Weightlifting App project',
           tech_used: 'HTML, CSS, JavaScript, Netlify',
           project_url: 'https://ui-weightlifting.netlify.com/',
-          github_repo: 'https://github.com/Build-Week-Water-My-Plants-01/Front-End'
+          github_repo: 'https://github.com/Weigth-Lifting-Journal-8/ui'
         },         
         
       ]);
@@ -95,46 +113,51 @@ exports.seed = function(knex) {
         },
         {
           id: 6,
+          skills_name: 'Python',
+          img_url: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg'
+        },
+        {
+          id: 7,
           skills_name: 'ReactJS',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
         },
         {
-          id: 7,
+          id: 8,
           skills_name: 'Redux',
           img_url: 'https://cdn.worldvectorlogo.com/logos/redux.svg'
         },
         {
-          id: 8,
+          id: 9,
           skills_name: 'React Router',
           img_url: 'https://seeklogo.com/images/R/react-router-logo-AB5BFB638F-seeklogo.com.png'
         },
         {
-          id: 9,
+          id: 10,
           skills_name: 'NodeJS',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
         },
         {
-          id: 10,
+          id: 11,
           skills_name: 'Express',
           img_url: 'https://seeklogo.com/images/E/express-js-logo-FA36FF1D3F-seeklogo.com.png'
         },
         {
-          id: 11,
+          id: 12,
           skills_name: 'SQLite',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/9/97/Sqlite-square-icon.svg'
         },
         {
-          id: 12,
+          id: 13,
           skills_name: 'PostgreSQL',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg'
         },
         {
-          id: 13,
+          id: 14,
           skills_name: 'Git',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg'
         },
         {
-          id: 14,
+          id: 15,
           skills_name: 'Github',
           img_url: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
         },
